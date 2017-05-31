d2d0750677bf02b23e990c476a76585e4939c96c
Thomas Soucie <tsoucie1858@gmail.com>
2017-05-31 14:28:31 -0600

    Commit outline of user_adjust_qunatity

--- spec/features/cart/user_can_adjust_cart_quantity_spec.rb
+++ spec/features/cart/user_can_adjust_cart_quantity_spec.rb
@@ -15,5 +15,21 @@ RSpec.feature "Visitor can adjust cart quantity" do
         description: "Presta valve 700x42c Bicyle Inner Tube",
         price: 11.99, status: 1)
     order = Order.create!()
+
+    visit cart_path
+
+    expect(page).to have_content(item.name)
+    expect(page).to have_content(order.item.quantity)
+
+    #select_quantity_increase
+    click_on("Update Cart")
+
+    expect(current_path).to eq(cart_path)
+    expect(page).to have_content(order.item.quantity + 1)
+    expect(page).to have_content(order.quantity + 1)
+
+
+
+
   end
 end
