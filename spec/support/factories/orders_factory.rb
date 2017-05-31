80b409d70d859768d7befa95acac05ee9986227d
adamgunther1 <adamgunther1@gmail.com>
2017-05-31 17:41:10 -0600

    adds commented attempt to add quantity in items orders join table

--- spec/support/factories/orders_factory.rb
+++ spec/support/factories/orders_factory.rb
@@ -1,12 +1,26 @@
+# FactoryGirl.define do
+#   factory :order_items do
+
+#   end
+# end
+
 FactoryGirl.define do
   factory :order do
     user
 
-    transient { item_count 1 }
+    # transient { item_count 1 }
 
-    after(:create) do |order, evaluator|
-      order.items << create_list(:item, evaluator.item_count)
-    end  
+    # after(:create) do |order, evaluator|
+    #   order.item_orders = create_list(
+    #     :item,
+    #     rand(3)+1,
+    #     order: order
+    #   )
+    #   # order.items << create_list(:item, evaluator.item_count)
+    #   # evaluator.item_count.times do |n|
+    #   #   order.item_orders << [item, n]
+    #   # end
+    # end  
   end
 
   trait :is_paid do
@@ -14,6 +28,7 @@ FactoryGirl.define do
       order.paid!
     end
   end
+
   trait :is_completed do
     after(:create) do |order|
       order.completed!
