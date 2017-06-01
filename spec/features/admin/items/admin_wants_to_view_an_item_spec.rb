50c0c2a3bbe8c01d2cfc4b9125301e131fe5910b
Aaron Olsen <mraaronolsen@gmail.com>
2017-06-01 16:10:24 -0600

    Admin feature spec for viewing items and an item

---
+++ spec/features/admin/items/admin_wants_to_view_an_item_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'Admin wants to view an item' do
+
+  before do
+    @items = create_list(:item, 3)
+    @admin = create(:user, :as_admin)
+
+    allow_any_instance_of(ApplicationController).to recieve(:current_user).and_return(admin)
+    visit admin_dashboard_path(@admin)
+  end
+
+  scenario 'admin can see items' do
+    click_link 'View All Items'
+
+    expect(page).to have_current_path(admin_items_path)
+
+    @items.each do |item|
+      expect(page).to have_link(@item.name)
+      expect(page).to have_content(@item.description)
+      expect(page).to have_content(@item.price)
+      expect(page).to have_content(@item.status)
+    end
+  end
+end
