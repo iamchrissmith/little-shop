bc4f6799468eb66f38cd13bff043604dc613d9f1
Aaron Olsen <mraaronolsen@gmail.com>
2017-06-01 14:40:39 -0600

    Admin feature spec for editing an item

--- spec/features/admin/items/admin_wants_to_edit_an_item_spec.rb
+++ spec/features/admin/items/admin_wants_to_edit_an_item_spec.rb
@@ -1,36 +1,35 @@
 require 'rails_helper'
 
-RSpec.describe 'Admin want to create and item' do
+RSpec.describe 'Admin want to edit and item' do
 
   before do
+    @item = create(:item)
     @admin = create(:user, :as_admin)
     @categories = create_list(:category, 3)
 
     allow_any_instance_of(ApplicationController).to recieve(:current_user).and_return(admin)
-    visit new_admin_item_path
+    visit edit_admin_item_path(@item)
   end
 
-  scenario 'admin creates valid item' do
-    expect(Item.count).to eq(0)
+  scenario 'admin edits valid item' do
 
     within 'form' do
       fill_in 'Name' with('Widget')
       fill_in 'Description' with('It bing bong bangs!')
       fill_in 'Price' with('3.5')
+      select 'Retire' from 'status'
       select @categories[1].name from 'categories'
 
       click_on 'Submit'
     end
 
-    expect(Item.count).to eq(1)
     expect(page).to have_current_path(admin_items_path)
-    expect(page).to have_content('Item Created')
+    expect(page).to have_content('Item Updated')
   end
 
-  scenario 'admin creates invalid item' do
+  scenario 'admin edits invalid item' do
 
     it 'does not enter a name' do
-      expect(Item.count).to eq(0)
 
       within 'form' do
         fill_in 'Name' with('')
@@ -41,7 +40,6 @@ RSpec.describe 'Admin want to create and item' do
         click_on 'Submit'
       end
 
-      expect(Item.count).to eq(0)
       expect(page).to have_content('Name cannot be blank')
     end
 
@@ -57,7 +55,6 @@ RSpec.describe 'Admin want to create and item' do
         click_on 'Submit'
       end
 
-      expect(Item.count).to eq(0)
       expect(page).to have_content('Name Widget already in use')
     end
 
@@ -72,11 +69,11 @@ RSpec.describe 'Admin want to create and item' do
         click_on 'Submit'
       end
 
-      expect(Item.count).to eq(0)
       expect(page).to have_content('Description cannot be blank')
     end
 
     it 'does not enter a price' do
+
       within 'form' do
         fill_in 'Name' with('Widget')
         fill_in 'Description' with('It bing bong bangs!')
@@ -86,11 +83,11 @@ RSpec.describe 'Admin want to create and item' do
         click_on 'Submit'
       end
 
-      expect(Item.count).to eq(0)
       expect(page).to have_content('Price cannot be blank')
     end
 
     it 'does not enter a valid price' do
+
       within 'form' do
         fill_in 'Name' with('Widget')
         fill_in 'Description' with('It bing bong bangs!')
@@ -100,7 +97,6 @@ RSpec.describe 'Admin want to create and item' do
         click_on 'Submit'
       end
 
-      expect(Item.count).to eq(0)
       expect(page).to have_content('Price must be a number') #This error might be different
     end
 
