dc43b3027def4395f684c8ed269331629db7da56
Chris Smith <42@chrisryansmith.com>
2017-06-01 17:57:30 -0600

    create user tables and new user view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :categories
+  helper_method :categories, :current_user
 
   def categories
     categories = Category.all
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
 end
