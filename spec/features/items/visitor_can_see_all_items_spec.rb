7e7f9e88e59f15bab2374b079b1b124b0574a92b
adamgunther1 <adamgunther1@gmail.com>
2017-06-01 10:35:22 -0600

    address syntax error in feature test for see all items

--- spec/features/items/visitor_can_see_all_items_spec.rb
+++ spec/features/items/visitor_can_see_all_items_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.feature "Visitor views all items (/items)" do
-  let! (:item_1) = create(:item)
-  let! (:item_2) = create(:item)
-  let! (:item_3) = create(:item)
+  let!(:item_1) { create(:item) }
+  let!(:item_2) { create(:item) }
+  let!(:item_3) { create(:item) }
 
   context "when not logged in" do
     scenario "it sees all the items" do
