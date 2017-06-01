462f29755fe5d213eeef636002f07e421f216669
Thomas Soucie <tsoucie1858@gmail.com>
2017-05-31 18:50:59 -0600

    Commit order factory implementation in user_can_adjust_cart_quantities_spec

--- spec/features/cart/user_can_adjust_cart_quantity_spec.rb
+++ spec/features/cart/user_can_adjust_cart_quantity_spec.rb
@@ -1,20 +1,12 @@
 require 'rails_helper'
-=begin
-As a visitor
-When I visit "/cart"
-Then I should see my item with a quantity of 1
-And when I increase the quantity
-Then my current page should be '/cart'
-And that item's quantity should reflect the increase
-And the subtotal for that item should increase
-And the total for the cart should match that increase
-=end
+
 RSpec.feature "Visitor can adjust cart quantity" do
   scenario "a visitor can change quantity of an item in their cart" do
-    item = Item.create!(name: "700x42c tube",
-        description: "Presta valve 700x42c Bicyle Inner Tube",
-        price: 11.99, status: 1)
-    order = Order.create!()
+    # item = Item.create!(name: "700x42c tube",
+    #     description: "Presta valve 700x42c Bicyle Inner Tube",
+    #     price: 11.99, status: 1)
+    # order = Order.create!()
+    let! (:order) = create(:order)
 
     visit cart_path
 
