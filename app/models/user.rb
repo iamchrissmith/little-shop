fd170a79f89ceb1f6486878e5e412a468abaec25
Chris Smith <42@chrisryansmith.com>
2017-06-01 18:03:04 -0600

    add models for user, city, state, address

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+
+  has_many :addresses
+  has_many :cities, through: :addresses
+  has_many :states, through: :cities
+  accept_nested_attributes_for :address, :city, :state
 end
