5f915f6b93c0b0216c7800d49c316e3771cfe532
Chris Smith <42@chrisryansmith.com>
2017-06-01 19:41:54 -0600

    add model validations to address

--- app/models/address.rb
+++ app/models/address.rb
@@ -5,4 +5,9 @@ class Address < ApplicationRecord
   has_one :state, through: :city
 
   accepts_nested_attributes_for :city
+
+  validates :city_id, presence: true
+  validates :user_id, presence: true
+  validates :address, presence: true
+  validates :zipcode, presence: true
 end
