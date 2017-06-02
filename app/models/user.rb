bc76f654eb40f6b02a1747a5f106379618584953
Chris Smith <42@chrisryansmith.com>
2017-06-01 18:13:16 -0600

    fix factories and add accepts_nested_attributes_for to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   has_many :addresses
   has_many :cities, through: :addresses
   has_many :states, through: :cities
-  accept_nested_attributes_for :address, :city, :state
+  accepts_nested_attributes_for :addresses, :cities, :states
 end
