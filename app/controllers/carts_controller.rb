c7bdf93ea02205b3f643e17e10ff8e44ef09706c
Thomas Soucie <tsoucie1858@gmail.com>
2017-06-03 15:46:16 -0600

    Remove pseudo and comments

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -6,6 +6,7 @@ class CartsController < ApplicationController
   before_action :item, only: %i[create update]
 
   def create
+<<<<<<< HEAD
     session[:cart] = update_cart(:add_item, id: item_id)
     flash[:notice] = "You now have #{pluralize(session[:cart][item_id], item.name)} in your cart."
     redirect_to session.delete(:return_to)
@@ -22,8 +23,7 @@ class CartsController < ApplicationController
   def show; end
 
   def destroy
-    id = params[:item_id].to_s
-    @cart.remove_item(id)
+    @cart.remove_item(item_id)
     session[:cart] = @cart.contents
     cart_link = "<a href=\"#{url_for(item)}\">#{item.name}</a>"
     flash[:notice] = "Successfully removed #{cart_link} from your cart"
@@ -43,17 +43,12 @@ class CartsController < ApplicationController
 
   def item_id
     params[:item_id].to_s
-    id = params[:item_id].to_s
-    item = Item.find(id)
-    @cart.add_item(id)
-    session[:cart] = @cart.contents
+=======
 
-    flash[:notice] = "You now have #{pluralize(session[:cart][id], item.name)} in your cart."
-    redirect_to root_path
+>>>>>>> Remove item from cart
   end
 
   def item
     Item.find(item_id)
   end
-
 end
