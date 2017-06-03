daec916cd66b4b327a205be0d842c3d158baa78b
Chris Smith <42@chrisryansmith.com>
2017-06-02 18:27:34 -0600

    remove addresses from user controller, views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,11 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
-    city = City.create(city_params) #find_by_name_or_create
-    if city.save
-      address = city.addresses.create(address_params)
-      @user.addresses << address
-    end
+    # city = City.create(city_params) #find_by_name_or_create
+    # if city.save
+    #   address = city.addresses.create(address_params)
+    #   @user.addresses << address
+    # end
     if @user.save
       session[:user_id] = @user.id
       flash[:success] = "Logged in as #{full_name(@user)}"
@@ -30,15 +30,15 @@ class UsersController < ApplicationController
     params.require(:user).permit(:first_name, :last_name, :email, :password)
   end
 
-  def address_params
-    params.require(:user).require(:address).permit(:address, :zipcode)
-  end
-
-  def city_params
-    params.require(:user)
-          .require(:address)
-          .require(:city).permit(:name, :state_id)
-  end
+  # def address_params
+  #   params.require(:user).require(:address).permit(:address, :zipcode)
+  # end
+  #
+  # def city_params
+  #   params.require(:user)
+  #         .require(:address)
+  #         .require(:city).permit(:name, :state_id)
+  # end
 
   def set_user
     @user = current_user
