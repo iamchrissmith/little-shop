4b999d93869fe8db041a5c864319d08c6c36d5a3
Chris Smith <42@chrisryansmith.com>
2017-06-03 10:21:41 -0600

    return cart create method to referrer

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,16 +1,27 @@
 class CartsController < ApplicationController
   include ActionView::Helpers::TextHelper
 
+  before_action :set_referrer, only: [:create]
+
   def create
+    # binding.pry
     id = params[:item_id].to_s
     item = Item.find(id)
-    @cart.add_item(id)
-    session[:cart] = @cart.contents
-
+    session[:cart] = update_cart(:add_item, id)
     flash[:notice] = "You now have #{pluralize(session[:cart][id], item.name)} in your cart."
-    redirect_to root_path
+    redirect_to session.delete(:return_to)
   end
 
-  def show
+  def show; end
+
+  private
+
+  def update_cart(action, id)
+    @cart.send(action, id)
+    @cart.contents
+  end
+
+  def set_referrer
+    session[:return_to] ||= request.referrer
   end
 end
--- spec/features/cart/user_can_adjust_cart_quantity_spec.rb
+++ spec/features/cart/user_can_adjust_cart_quantity_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor can adjust cart quantity" do
-  describe "a visitor can change quantity of an item in their cart" do
-    let (:item) { create(:item) }
-    let (:cart) { build(:cart, contents: {"#{item.id}" => 1}) }
+RSpec.feature 'Visitor can adjust cart quantity' do
+  describe 'a visitor can change quantity of an item in their cart' do
+    let(:item) { create(:item) }
+    let(:cart) { build(:cart, contents: { item.id.to_s => 1 }) }
 
-    scenario "increase quantity in the cart" do
+    scenario 'increase quantity in the cart' do
       @cart = cart
 
       allow_any_instance_of(ApplicationController).to receive(:current_cart).and_return(cart)
@@ -13,18 +13,18 @@ RSpec.feature "Visitor can adjust cart quantity" do
       visit cart_path
 
       expect(page).to have_content(item.name)
-      expect(page).to have_content("Quantity: 1")
+      expect(page).to have_content('Quantity: 1')
       expect(page).to have_content("Total: $#{item.price}")
 
-      #select_quantity_increase
-      click_on("Update Cart")
+      # select_quantity_increase
+      click_on('Update Cart')
 
       expect(current_path).to eq(cart_path)
       expect(page).to have_content(order.item.quantity + 1)
       expect(page).to have_content(order.quantity + 1)
     end
 
-    xscenario "decrease quantity in the cart" do
+    xscenario 'decrease quantity in the cart' do
 
       visit_cart_path
 
@@ -32,8 +32,8 @@ RSpec.feature "Visitor can adjust cart quantity" do
       expect(page).to have_content(order.item.quantity)
       expect(page).to have_content(order.quantity)
 
-      #select_quantity_increase
-      click_on("Update Cart")
+      # select_quantity_increase
+      click_on('Update Cart')
 
       expect(current_path).to eq(cart_path)
       expect(page).to have_content(order.item.quantity - 1)
