f9e9fdf819c94ffe27ba80beccb346fba8ef8aa1
Thomas Soucie <tsoucie1858@gmail.com>
2017-06-05 16:25:47 -0600

    Create Migration AddPhotoToItem and add image to spec and _item partial

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,12 @@ class Item < ApplicationRecord
   has_many :categories, through: :item_categories
   has_many :order_items
   has_many :orders, through: :order_items
+  has_attached_file :photo, styles: {
+    thumb: '300x300>',
+    square:'450x450>',
+  }
 
+  validates_content_type :photo, :content_type => /\Aimage\/.*\Z/
   validates_presence_of :name, :description, :price, :categories
   validates_uniqueness_of :name
   validates_numericality_of :price
