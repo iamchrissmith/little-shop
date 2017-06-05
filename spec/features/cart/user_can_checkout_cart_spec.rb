3f6a2fb6444e14d6af1480df2d8097cbea2fe2a1
Chris Smith <42@chrisryansmith.com>
2017-06-05 16:34:55 -0600

    redirect visitors accessing order pages to login page

--- spec/features/cart/user_can_checkout_cart_spec.rb
+++ spec/features/cart/user_can_checkout_cart_spec.rb
@@ -5,7 +5,6 @@ RSpec.feature "User can checkout with cart" do
   let(:item_2) { create(:item, price: 100) }
   let(:user) { create(:user) }
   let!(:state) { create(:state) }
-  # let!(:address) { create(:address) }
 
   context "when the user is logged in" do
     scenario "the user can checkout" do
@@ -81,59 +80,6 @@ RSpec.feature "User can checkout with cart" do
       end
 
       expect(page).to have_content("Total: $#{total}")
-    end
-  end
-
-  context "when the user is logged in and has previous orders" do
-    let!(:previous_order) { create(:order, user: user, items: [item_1, item_2, item_2, item_2]) }
-    let!(:current_order) { create(:order, user: user, items: [item_1, item_1, item_2]) }
-
-    scenario "the user can see both orders on /orders" do
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-      previous_order.completed!
-
-      visit orders_path
-      expect(page).to have_content 'Your purchases'
-
-      expect(page).to have_content 'Your information'
-      expect(page).to have_content full_name(user)
-      expect(page).to have_content user.email
-
-      expect(page).to have_css(".order-#{current_order.id}.well-lg")
-
-      within(".order-#{current_order.id}") do
-        expect(page).to have_content "Order Status: #{current_order.status}"
-        within(".item-#{item_1.id}") do
-          expect(page).to have_content item_1.name
-          expect(page).to have_content item_1.description
-          expect(page).to have_content item_1.price
-          expect(page).to have_content('2')
-        end
-        within(".item-#{item_2.id}") do
-          expect(page).to have_content item_2.name
-          expect(page).to have_content item_2.description
-          expect(page).to have_content item_2.price
-          expect(page).to have_content('1')
-        end
-        expect(page).to have_content("Total: $#{current_order.total_price}")
-      end
-
-      within(".order-#{previous_order.id}") do
-        expect(page).to have_content "Order Status: completed"
-        within(".item-#{item_1.id}") do
-          expect(page).to have_content item_1.name
-          expect(page).to have_content item_1.description
-          expect(page).to have_content item_1.price
-          expect(page).to have_content('1')
-        end
-        within(".item-#{item_2.id}") do
-          expect(page).to have_content item_2.name
-          expect(page).to have_content item_2.description
-          expect(page).to have_content item_2.price
-          expect(page).to have_content('3')
-        end
-        expect(page).to have_content("Total: $#{previous_order.total_price}")
-      end
     end
   end
 end
