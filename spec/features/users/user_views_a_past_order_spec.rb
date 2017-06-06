0b24c6da05f9cb6a9449f4655f5b1c4783404a3f
Chris Smith <42@chrisryansmith.com>
2017-06-06 16:48:50 -0600

    include date/time order was placed at

--- spec/features/users/user_views_a_past_order_spec.rb
+++ spec/features/users/user_views_a_past_order_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.feature 'Logged In User can View Past Orders' do
   let(:user) { create(:user) }
   scenario 'a visitor can view past orders' do
-    user.orders << create_list(:order, 3)
+    user.orders << create_list(:order, 3, :with_items)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
@@ -19,7 +19,14 @@ RSpec.feature 'Logged In User can View Past Orders' do
 
     expect(current_path).to eq("/orders/#{user.orders[0].id}")
     expect(page).to have_content("Order #{user.orders[0].id}")
-
-    #how to check for listed out items?
+    expect(page).to have_content("Placed at: #{user.orders[0].placed_at}")
+    user.orders[0].items.each do |item|
+      within(".item-#{item.id}") do
+        expect(page).to have_link item.name, href: item_path(item)
+        expect(page).to have_content item.description
+        expect(page).to have_content item.price
+        expect(page).to have_content '1'
+      end
+    end
   end
 end
