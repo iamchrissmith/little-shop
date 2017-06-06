dc4f0881c2b8c50b3a523ca33f1f3f0faa0afe73
Aaron Olsen <mraaronolsen@gmail.com>
2017-06-06 11:42:43 -0600

    Fixed changed items test

--- spec/features/items/visitor_can_see_all_items_spec.rb
+++ spec/features/items/visitor_can_see_all_items_spec.rb
@@ -4,14 +4,13 @@ RSpec.feature "Visitor views all items (/items)" do
   let!(:item_1) { create(:item) }
   let!(:item_2) { create(:item) }
   let!(:item_3) { create(:item) }
-  
+
 
   context "when not logged in" do
     scenario "it sees all the items" do
 
       visit items_path
 
-      expect(page).to have_content "All Items"
       expect(page).to have_link item_1.name, href: item_path(item_1)
       # expect(page).to have_link "Add to Cart", href: cart_path(item_id: item_1.id)
       expect(page).to have_link item_2.name, href: item_path(item_2)
