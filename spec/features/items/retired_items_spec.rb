ea8e0a69816680ad887bcd29869e33396b6a3e00
Chris Smith <42@chrisryansmith.com>
2017-06-06 14:54:02 -0600

    clean up item retired view tests

--- spec/features/items/retired_items_spec.rb
+++ spec/features/items/retired_items_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.feature "User can see retired items" do
-  let! (:item) { create(:item, :is_retired) }
+  let! (:item) { create(:item, status: 'retired') }
   context "as a user" do
-    xscenario "it can view a retired item" do
+    scenario "it can view a retired item" do
 
       visit item_path(item)
 
       expect(current_path).to eq(item_path(item))
-      expect(page).to not_have_content("Add to Cart")
+      expect(page).not_to have_content("Add to Cart")
       expect(page).to have_content("Item Retired")
 
     end
