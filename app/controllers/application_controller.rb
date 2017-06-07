0931c658b3c63dc1d37c726c3f506a32eafef555
adamgunther1 <adamgunther1@gmail.com>
2017-06-06 20:36:01 -0600

    fixes flash formatting issue of size and color

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,8 +24,8 @@ class ApplicationController < ActionController::Base
       case level
           when 'notice' then "alert alert-success"
           when 'success' then "alert alert-success"
-          when 'error' then "alert alert-error"
-          when 'alert' then "alert alert-error"
+          when 'error' then "alert alert-danger"
+          when 'alert' then "alert alert-danger"
       end
   end
 
